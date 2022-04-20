2600f7ee93053c4bca2c58a8197472ecc5f16e7b
zhouniy <104041394+zhouniy@users.noreply.github.com>
2022-04-20 15:56:21 +0800

    Update Student.cs

--- Midmester/Models/Student.cs
+++ Midmester/Models/Student.cs
@@ -4,15 +4,30 @@ namespace Midmester.Models
 {
     public class Student
     {
-        public int Id { get; set; }
+        [Display(Name = "姓名")]
+        [StringLength(300)]
         public string? Name { get; set; }
+
+        [Display(Name = "性别")]
+        [StringLength(1)]
         public string? Sex { get; set; }
+
+        [Display(Name = "年龄")]
+        [Range(0, 200)]
         public string? Age { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime? Birthday { get; set; }
-        public string? phone { get; set; }
-        public string? Temp { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime? Date { get; set; }
+
+        [Display(Name = "出生日期")]
+        public DateTime Birth { get; set; }
+
+        [Display(Name = "手机")]
+        [StringLength(13)]
+        public string? Phone { get; set; }
+
+        [Display(Name = "体温")]
+        [Range(30, 50)]
+        public decimal Temperature { get; set; }
+
+        [Display(Name = "填表日期")]
+        public DateTime Date { get; set; }
     }
 }
